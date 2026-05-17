efa050079f618b170fd1ea2123f02bd0cddedb78
Rasabattula-DurgaNaresh <rdurganaresh88@outlook.com>
2026-05-17 09:51:25 +0530

    added

---
+++ Java8/java-8-lambdas-exercises-master/src/main/java/com/insightfullogic/java8/examples/chapter4/MusicalCarriage.java
@@ -0,0 +1,12 @@
+package com.insightfullogic.java8.examples.chapter4;
+
+// BEGIN body
+public class MusicalCarriage implements Carriage, Jukebox {
+
+  @Override
+  public String rock() {
+    return Carriage.super.rock();
+  }
+
+}
+// END body
